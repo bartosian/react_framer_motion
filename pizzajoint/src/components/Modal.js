6f93b1cdc93a89e9c6f1f93660298ac8b014048c
bartosian <bartosian1989@gmail.com>
2020-10-26 08:33:00 -0400

    Added animation to modal

--- pizzajoint/src/components/Modal.js
+++ pizzajoint/src/components/Modal.js
@@ -9,6 +9,20 @@ const backdrop = {
   hidden: { opacity: 0 }
 }
 
+const modal = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0
+  },
+  visible: {
+    y: "200px",
+    opacity: 1,
+    transition: {
+      delay: 0.5
+    }
+  }
+}
+
 const Modal = ({ showModal, setShowModal }) => {
   return (
     <AnimatePresence exitBeforeEnter>
@@ -18,8 +32,18 @@ const Modal = ({ showModal, setShowModal }) => {
             variants={backdrop}
             initial="hidden"
             animate="visible"
+            exit="hidden"
           >
-
+            <motion.div
+              className="modal"
+              variants={modal}>
+              <p>Want to make another pizza?</p>
+              <Link to="/">
+                <button
+                  onClick={() => setShowModal(false)}
+                >Start Again</button>
+              </Link>
+            </motion.div>
           </motion.div>
       )}
     </AnimatePresence>
